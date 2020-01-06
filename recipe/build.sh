7008c9119668253d17b5c74638ea4f38fda058e5
Danilo Horta <danilo.horta@pm.me>
2020-01-06 10:38:54 +0000

    - conda inspect

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,7 @@ cmake \
      -DCMAKE_BUILD_TYPE=Release \
      -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
      ..
+
 cmake --build . --config Release
 cmake --build . --config Release --target test
 cmake --build . --config Release --target install
