9fb551df6afa12a36a45eb8176375fe8730d4b06
Danilo Horta <danilo.horta@pm.me>
2020-01-06 10:01:01 +0000

    0.4.1

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,13 @@
-pushd . && mkdir build && cd build
-if errorlevel 1 exit 1
+mkdir build && cd build
 
-cmake -G "%CMAKE_GENERATOR%" ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
+cmake -GNinja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     %SRC_DIR%
-if errorlevel 1 exit 1
 
+cmake --build . --config Release
+ctest --output-on-failure -C Release
 cmake --build . --config Release --target install
-if errorlevel 1 exit 1
 
-ctest -V --output-on-failure -C Release
-if errorlevel 1 exit 1
-
-popd && rd /q /s build
-if errorlevel 1 exit 1
+cd %SRC_DIR% && rd /q /s build
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -ex
 
-export CFLAGS="${CFLAGS} -O3 -fPIC"
-export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
+set -ex
 
 mkdir build && cd build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX $SRC_DIR
-make -j$CPU_COUNT && make test && make install
+cmake \
+     -DCMAKE_BUILD_TYPE=Release \
+     -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+     ..
+cmake --build . --config Release
+cmake --build . --config Release --target test
+cmake --build . --config Release --target install
