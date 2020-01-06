cfb1975717b794c31c30f10d6459bf28642af244
Danilo Horta <danilo.horta@pm.me>
2020-01-06 11:05:05 +0000

    syntax error

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,8 @@ cmake -GNinja ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DBUILD_SHARED_LIBS=ON \
-    %SRC_DIR%
+    -DBUILD_SHARED_LIBS=ON ^
+    "%SRC_DIR%"
 
 cmake --build . --config Release
 ctest --output-on-failure -C Release
